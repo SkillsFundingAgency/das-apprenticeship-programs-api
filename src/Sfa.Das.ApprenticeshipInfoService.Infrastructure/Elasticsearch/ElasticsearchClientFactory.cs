0eb592def37b1e8b6cd588f41143c3f0b4a4242c
Josevi Agullo <josevi.agullo@gmail.com>
2017-09-13 10:19:14 +0100

    CI-383 Connecting to elk5

--- src/Sfa.Das.ApprenticeshipInfoService.Infrastructure/Elasticsearch/ElasticsearchClientFactory.cs
+++ src/Sfa.Das.ApprenticeshipInfoService.Infrastructure/Elasticsearch/ElasticsearchClientFactory.cs
@@ -1,7 +1,10 @@
 ﻿using Elasticsearch.Net;
+using FeatureToggle.Core.Fluent;
 using Nest;
 using Sfa.Das.ApprenticeshipInfoService.Core.Configuration;
 using Sfa.Das.ApprenticeshipInfoService.Core.Models;
+using Sfa.Das.ApprenticeshipInfoService.Infrastructure.Extensions;
+using Sfa.Das.ApprenticeshipInfoService.Infrastructure.FeatureToggles;
 
 namespace Sfa.Das.ApprenticeshipInfoService.Infrastructure.Elasticsearch
 {
@@ -16,16 +19,27 @@ namespace Sfa.Das.ApprenticeshipInfoService.Infrastructure.Elasticsearch
 
         public IElasticClient Create()
         {
-            using (var settings = new ConnectionSettings(new StaticConnectionPool(_applicationSettings.ElasticServerUrls)))
+            ConnectionSettings settings;
+            if (Is<IgnoreSslCertificateFeature>.Enabled)
             {
-                settings.DisableDirectStreaming();
-                settings.MapDefaultTypeNames(d => d.Add(typeof(StandardSearchResultsItem), "standarddocument"));
-                settings.MapDefaultTypeNames(d => d.Add(typeof(FrameworkSearchResultsItem), "frameworkdocument"));
-                settings.MapDefaultTypeNames(d => d.Add(typeof(StandardProviderSearchResultsItem), "standardprovider"));
-                settings.MapDefaultTypeNames(d => d.Add(typeof(FrameworkProviderSearchResultsItem), "frameworkprovider"));
-
-                return new ElasticClient(settings);
+                settings = new ConnectionSettings(
+                    new StaticConnectionPool(_applicationSettings.ElasticServerUrls),
+                    new MyCertificateIgnoringHttpConnection());
             }
+            else
+            {
+                settings = new ConnectionSettings(
+                    new StaticConnectionPool(_applicationSettings.ElasticServerUrls));
+            }
+
+            settings.BasicAuthentication(_applicationSettings.ElasticsearchUsername, _applicationSettings.ElasticsearchPassword);
+            settings.DisableDirectStreaming();
+            settings.MapDefaultTypeNames(d => d.Add(typeof(StandardSearchResultsItem), "standarddocument"));
+            settings.MapDefaultTypeNames(d => d.Add(typeof(FrameworkSearchResultsItem), "frameworkdocument"));
+            settings.MapDefaultTypeNames(d => d.Add(typeof(StandardProviderSearchResultsItem), "standardprovider"));
+            settings.MapDefaultTypeNames(d => d.Add(typeof(FrameworkProviderSearchResultsItem), "frameworkprovider"));
+
+            return new ElasticClient(settings);
         }
     }
 }
