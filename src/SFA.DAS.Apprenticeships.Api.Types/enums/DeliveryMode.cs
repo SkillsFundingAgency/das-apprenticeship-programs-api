67358fa1327012f67ea3136299e1972e4435836d
MarkFCain <mark.cain@alimar.co.uk>
2017-11-07 13:33:12 +0000

    CI-410 fixed enum to string conversion and tests added

--- src/SFA.DAS.Apprenticeships.Api.Types/enums/DeliveryMode.cs
+++ src/SFA.DAS.Apprenticeships.Api.Types/enums/DeliveryMode.cs
@@ -1,11 +1,17 @@
-﻿using System.ComponentModel;
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace SFA.DAS.Apprenticeships.Api.Types.enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum DeliveryMode
     {
-        [Description("DayRelease")] DayRelease,
-        [Description("BlockRelease")] BlockRelease,
-        [Description("100PercentEmployer")] HundredPercentEmployer
+        [EnumMember(Value = "DayRelease")]
+        DayRelease,
+        [EnumMember(Value = "BlockRelease")]
+        BlockRelease,
+        [EnumMember(Value = "100PercentEmployer")]
+        HundredPercentEmployer            
     }
 }
