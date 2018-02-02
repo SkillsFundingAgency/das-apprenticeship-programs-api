5ee2ba217c80a3b91c85d238d7096b44959637d5
MarkFCain <mark.cain@alimar.co.uk>
2018-02-02 15:30:29 +0000

    CI-574 first basic repo test added

--- src/Sfa.Das.ApprenticeshipInfoService.UnitTests/Repositories/ProviderRepositoryTests.cs
+++ src/Sfa.Das.ApprenticeshipInfoService.UnitTests/Repositories/ProviderRepositoryTests.cs
@@ -12,6 +12,8 @@ using Sfa.Das.ApprenticeshipInfoService.Core.Models;
 using Sfa.Das.ApprenticeshipInfoService.Infrastructure.Elasticsearch;
 using Sfa.Das.ApprenticeshipInfoService.Infrastructure.Mapping;
 using Sfa.Das.ApprenticeshipInfoService.Infrastructure.Models;
+using SFA.DAS.Apprenticeships.Api.Types;
+using SFA.DAS.Apprenticeships.Api.Types.Pagination;
 using SFA.DAS.Apprenticeships.Api.Types.Providers;
 using SFA.DAS.NLog.Logger;
 
@@ -21,7 +23,7 @@ namespace Sfa.Das.ApprenticeshipInfoService.UnitTests.Repositories
     public class ProviderRepositoryTests
     {
         private const int TakeMaximum = 3;
-        private const int PageSizeApprenticeshipSummary = 2;
+        private const int PageSizeApprenticeshipSummary = 4;
         private Mock<IElasticsearchCustomClient> _elasticClient;
 
         private Mock<ILog> _log;
@@ -62,7 +64,7 @@ namespace Sfa.Das.ApprenticeshipInfoService.UnitTests.Repositories
             var repo = new ProviderRepository(
                 _elasticClient.Object,
                 _log.Object,
-                _mockConfigurationSettings.Object,
+                Mock.Of<IConfigurationSettings>(),
                 Mock.Of<IProviderLocationSearchProvider>(),
                 Mock.Of<IProviderMapping>(),
                 _queryHelper.Object,
@@ -256,7 +258,7 @@ namespace Sfa.Das.ApprenticeshipInfoService.UnitTests.Repositories
             _log.Verify(x => x.Warn($"httpStatusCode was {(int)HttpStatusCode.Ambiguous} when querying provider standards for ukprn [{ukprn}]"), Times.Once);
 
         }
-
+        
         [Test]
         public void GetProviderStandardsByUkprnShouldLogWhenApprenticeshipDetailsDoNotRespondAsExpected()
         {
@@ -388,5 +390,121 @@ namespace Sfa.Das.ApprenticeshipInfoService.UnitTests.Repositories
 
             _log.Verify(x => x.Warn(It.IsAny<string>()), Times.Once);
         }
+
+        [Test]
+        public void ShouldReturnActiveListOfProviderApprenticeshipsForUkprnInExpectedOrder()
+        {
+            const long ukprn = 10005214L;
+            var searchResponseForDtos = new Mock<ISearchResponse<ProviderStandardDto>>();
+            var apiCallForStandards = new Mock<IApiCallDetails>();
+            var apiCallForDtos = new Mock<IApiCallDetails>();
+            var searchResponse = new Mock<ISearchResponse<ProviderStandard>>();
+            var apiCallForFrameworks = new Mock<IApiCallDetails>();
+            var searchResponseForFrameworkDtos = new Mock<ISearchResponse<ProviderFrameworkDto>>();
+            var apiCallForFrameworkDtos = new Mock<IApiCallDetails>();
+            var searchResponseForFrameworks = new Mock<ISearchResponse<ProviderFramework>>();    
+
+            searchResponseForDtos.Setup(x => x.Documents).Returns(new List<ProviderStandardDto> { new ProviderStandardDto() });
+            searchResponseForFrameworkDtos.Setup(x => x.Documents).Returns(new List<ProviderFrameworkDto>());
+
+            var configurationSettings = new Mock<IConfigurationSettings>();
+            configurationSettings.Setup(x => x.TakeMaximum).Returns(10);
+            apiCallForDtos.SetupGet(x => x.HttpStatusCode).Returns((int)HttpStatusCode.OK);
+            searchResponseForDtos.SetupGet(x => x.ApiCall).Returns(apiCallForDtos.Object);
+            apiCallForStandards.SetupGet(x => x.HttpStatusCode).Returns((int)HttpStatusCode.OK);
+            apiCallForFrameworks.SetupGet(x => x.HttpStatusCode).Returns((int)HttpStatusCode.OK);
+            apiCallForFrameworkDtos.SetupGet(x => x.HttpStatusCode).Returns((int)HttpStatusCode.OK);
+            searchResponse.SetupGet(x => x.ApiCall).Returns(apiCallForStandards.Object);
+            searchResponseForFrameworks.SetupGet(x => x.ApiCall).Returns(apiCallForFrameworks.Object);
+            searchResponseForFrameworkDtos.SetupGet(x => x.ApiCall).Returns(apiCallForFrameworkDtos.Object);
+
+            var providerStandardArcheologistLev1 = new ProviderStandard { StandardId = 20, Title = "Archeologist", Level = 1, EffectiveFrom = DateTime.Today.AddDays(-3) };
+            var providerStandardZebraWranglerShouldBeCutOffByProviderApprenticeshipTrainingMaximum
+                = new ProviderStandard() { StandardId = 10, Title = "Zebra Wrangler", Level = 1, EffectiveFrom = DateTime.Today.AddDays(-3) };
+
+            var providerStandardWithNoEffectiveFrom = new ProviderStandard { StandardId = 30, Title = "Absent because no effective from date", Level = 4, EffectiveFrom = null };
+
+            var standards = new List<ProviderStandard>
+            {
+                providerStandardZebraWranglerShouldBeCutOffByProviderApprenticeshipTrainingMaximum,
+                providerStandardArcheologistLev1,
+                providerStandardWithNoEffectiveFrom
+
+            };
+
+            searchResponse.Setup(x => x.Documents).Returns(standards);
+
+            var providerFrameworkAccountingLev3 = new ProviderFramework { FrameworkId = "321-1-1", PathwayName = "Accounting", Level = 3, EffectiveFrom = DateTime.Today.AddDays(-3) };
+            var providerFrameworkAccountingLev2 = new ProviderFramework { FrameworkId = "321-2-1", PathwayName = "Accounting", Level = 2, EffectiveFrom = DateTime.Today.AddDays(-3), EffectiveTo = DateTime.Today.AddDays(2) };
+            var providerFrameworkNoLongerActive = new ProviderFramework { FrameworkId = "234-3-2", PathwayName = "Active in the past", Level = 4, EffectiveFrom = DateTime.MinValue, EffectiveTo = DateTime.Today.AddDays(-2) };
+
+            var frameworks = new List<ProviderFramework>
+            {
+                providerFrameworkAccountingLev3,
+                providerFrameworkAccountingLev2,
+                providerFrameworkNoLongerActive
+            };
+        searchResponseForFrameworks.Setup(x => x.Documents).Returns(frameworks);
+
+            _mockActiveFrameworkChecker
+                .Setup(x => x.CheckActiveFramework(It.IsAny<string>(), It.IsAny<DateTime?>(), It.IsAny<DateTime?>()))
+                .Returns(true);
+            _mockActiveFrameworkChecker
+                .Setup(x => x.CheckActiveFramework("234-3-2", It.IsAny<DateTime?>(), It.IsAny<DateTime?>()))
+                .Returns(false);
+            _mockActiveFrameworkChecker
+                .Setup(x => x.CheckActiveStandard(It.IsAny<string>(), It.IsAny<DateTime?>(), It.IsAny<DateTime?>()))
+                .Returns(true);
+            _mockActiveFrameworkChecker
+                .Setup(x => x.CheckActiveStandard(It.IsAny<string>(), null, It.IsAny<DateTime?>()))
+                .Returns(false);
+
+            _elasticClient.Setup(x => x.Search(It.IsAny<Func<SearchDescriptor<ProviderStandardDto>, ISearchRequest>>(), It.IsAny<string>())).Returns(searchResponseForDtos.Object);
+            _elasticClient.Setup(x => x.Search(It.IsAny<Func<SearchDescriptor<ProviderStandard>, ISearchRequest>>(), It.IsAny<string>())).Returns(searchResponse.Object);
+            _elasticClient.Setup(x => x.Search(It.IsAny<Func<SearchDescriptor<ProviderFrameworkDto>, ISearchRequest>>(), It.IsAny<string>())).Returns(searchResponseForFrameworkDtos.Object);
+            _elasticClient.Setup(x => x.Search(It.IsAny<Func<SearchDescriptor<ProviderFramework>, ISearchRequest>>(), It.IsAny<string>())).Returns(searchResponseForFrameworks.Object);
+
+            var paginationDetails = new PaginationDetails
+            {
+                LastPage = 1,
+                NumberOfRecordsToSkip = 0,
+                NumberPerPage = 20,
+                Page = 0,
+                TotalCount = 3
+            };
+
+            _mockPaginationHelper
+                .Setup(x => x.GeneratePaginationDetails(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).Returns(paginationDetails);
+
+            var repo = new ProviderRepository(
+                _elasticClient.Object,
+                _log.Object,
+                _mockConfigurationSettings.Object,
+                Mock.Of<IProviderLocationSearchProvider>(),
+                Mock.Of<IProviderMapping>(),
+                _queryHelper.Object,
+                _mockActiveFrameworkChecker.Object,
+                _mockPaginationHelper.Object);
+
+            var result = repo.GetActiveApprenticeshipTrainingByProvider(ukprn, 1);
+
+            var providerApprenticeships = result.ApprenticeshipTrainingItems.ToArray();
+            Assert.AreEqual(PageSizeApprenticeshipSummary, providerApprenticeships.Length);
+
+            Assert.AreEqual(providerApprenticeships[0].Identifier, providerFrameworkAccountingLev2.FrameworkId,
+                $"Expect first item to be Framework Id [{providerFrameworkAccountingLev2.FrameworkId}], but was [{providerApprenticeships[0].Identifier} ]");
+            Assert.AreEqual(providerApprenticeships[1].Identifier, providerFrameworkAccountingLev3.FrameworkId);
+            Assert.AreEqual(providerApprenticeships[1].TrainingType, ApprenticeshipTrainingType.Framework);
+            Assert.AreEqual(providerApprenticeships[1].Type, "Framework");
+            Assert.AreEqual(providerApprenticeships[1].Level, 3);
+            Assert.AreEqual(providerApprenticeships[1].Name, "Accounting");
+            Assert.AreEqual(providerApprenticeships[2].Identifier, providerStandardArcheologistLev1.StandardId.ToString());
+            Assert.AreEqual(providerApprenticeships[2].TrainingType, ApprenticeshipTrainingType.Standard);
+            Assert.AreEqual(providerApprenticeships[2].Type, "Standard");
+            Assert.AreEqual(providerApprenticeships[2].Level, 1);
+            Assert.AreEqual(providerApprenticeships[2].Name, "Archeologist");
+
+        }
+
     }
 }
