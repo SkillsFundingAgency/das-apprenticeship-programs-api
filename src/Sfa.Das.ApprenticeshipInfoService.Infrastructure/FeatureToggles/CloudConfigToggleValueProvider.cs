9802b7fe4d819db501f4ff4ec77ef8b6ae05f463
josevi.agullo <josevi.agullo@gmail.com>
2018-03-07 13:00:16 +0000

    Now reading FeatureToggle from CloudConfiguration

---
+++ src/Sfa.Das.ApprenticeshipInfoService.Infrastructure/FeatureToggles/CloudConfigToggleValueProvider.cs
@@ -0,0 +1,13 @@
+﻿using FeatureToggle.Core;
+using Microsoft.Azure;
+
+namespace Sfa.Das.ApprenticeshipInfoService.Infrastructure.FeatureToggles
+{
+    public class CloudConfigToggleValueProvider : IBooleanToggleValueProvider
+    {
+        public bool EvaluateBooleanToggleValue(IFeatureToggle toggle)
+        {
+            return bool.Parse(CloudConfigurationManager.GetSetting($"FeatureToggle.{toggle.GetType().Name}"));
+        }
+    }
+}
