f10f3d4efac8c977fa093eb5d75b8db602f6d12f
josevi.agullo <josevi.agullo@gmail.com>
2018-03-21 16:31:34 +0000

    CI-677 Adding apprenticeship search endpoint and replicating logic from FAT website

---
+++ src/Sfa.Das.ApprenticeshipInfoService.Api/Controllers/SearchController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Sfa.Das.ApprenticeshipInfoService.Core.Models;
+
+namespace Sfa.Das.ApprenticeshipInfoService.Api.Controllers
+{
+    using System;
+    using System.Net;
+    using System.Web.Http;
+    using Sfa.Das.ApprenticeshipInfoService.Api.Attributes;
+    using Sfa.Das.ApprenticeshipInfoService.Core.Services;
+    using SFA.DAS.NLog.Logger;
+    using Swashbuckle.Swagger.Annotations;
+
+    public class SearchController : ApiController
+    {
+        private readonly IApprenticeshipSearchService _apprenticeshipSearchService;
+        private readonly ILog _logger;
+
+        public SearchController(IApprenticeshipSearchService apprenticeshipSearchService, ILog logger)
+        {
+            _apprenticeshipSearchService = apprenticeshipSearchService;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Search all apprenticeships
+        /// </summary>
+        /// <returns>a search result object</returns>
+        [SwaggerOperation("GetAllActiveStandards")]
+        [SwaggerResponse(HttpStatusCode.OK, "OK", typeof(IEnumerable<ApprenticeshipSearchResultsItem>))]
+        [Route("searchApprenticeships/{keywords}/{page}/{take}")]
+        [ExceptionHandling]
+        public IEnumerable<ApprenticeshipSearchResultsItem> Get(string keywords, int page, int take)
+        {
+            try
+            {
+                var response = _apprenticeshipSearchService.SearchApprenticeships(keywords, page, take);
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "/search");
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Sfa.Das.ApprenticeshipInfoService.Infrastructure/Elasticsearch/ApprenticeshipSearchService.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using FeatureToggle.Core.Fluent;
+using Sfa.Das.ApprenticeshipInfoService.Infrastructure.FeatureToggles;
+using SFA.DAS.Apprenticeships.Api.Types;
+
+namespace Sfa.Das.ApprenticeshipInfoService.Infrastructure.Elasticsearch
+{
+    using System.Linq;
+    using Core.Configuration;
+    using Core.Models;
+    using Core.Services;
+    using Mapping;
+    using Nest;
+
+    public sealed class ApprenticeshipSearchService : IApprenticeshipSearchService
+    {
+        private readonly IElasticsearchCustomClient _elasticsearchCustomClient;
+        private readonly IConfigurationSettings _applicationSettings;
+        private readonly IStandardMapping _standardMapping;
+        private readonly IQueryHelper _queryHelper;
+
+        public ApprenticeshipSearchService(
+            IElasticsearchCustomClient elasticsearchCustomClient,
+            IConfigurationSettings applicationSettings,
+            IStandardMapping standardMapping,
+            IQueryHelper queryHelper)
+        {
+            _elasticsearchCustomClient = elasticsearchCustomClient;
+            _applicationSettings = applicationSettings;
+            _standardMapping = standardMapping;
+            _queryHelper = queryHelper;
+        }
+
+        public List<ApprenticeshipSearchResultsItem> SearchApprenticeships(string keywords, int page, int take)
+        {
+            var takeElements = take == 0 ? 10 : take;
+
+            var formattedKeywords = _queryHelper.FormatKeywords(keywords);
+
+            var searchDescriptor = GetSearchDescriptor(page, takeElements, formattedKeywords);
+
+            var results = _elasticsearchCustomClient.Search<ApprenticeshipSearchResultsItem>(s => searchDescriptor);
+
+            return results.Documents.ToList();
+        }
+
+        private SearchDescriptor<ApprenticeshipSearchResultsItem> GetSearchDescriptor(
+            int page, int take, string formattedKeywords)
+        {
+            return formattedKeywords == "*"
+                ? GetAllSearchDescriptor(page, take, formattedKeywords)
+                : GetKeywordSearchDescriptor(page, take, formattedKeywords);
+        }
+
+        private SearchDescriptor<ApprenticeshipSearchResultsItem> GetAllSearchDescriptor(
+            int page, int take, string formattedKeywords)
+        {
+            var skip = (page - 1) * take;
+
+            var searchDescriptor = new SearchDescriptor<ApprenticeshipSearchResultsItem>()
+                .Index(_applicationSettings.ApprenticeshipIndexAlias)
+                .AllTypes()
+                .Skip(skip)
+                .Take(take)
+                .Query(q => q
+                    .Bool(b => b
+                    .Filter(PublishedApprenticeship())
+                    .Must(
+                        MustBeStartedApprenticeship(),
+                        MustBeNonExpiredApprenticceship(),
+                        m => m
+                            .QueryString(qs => qs
+                                .Fields(fs => fs
+                                    .Field(f => f.Title)
+                                    .Field(p => p.JobRoles)
+                                    .Field(p => p.Keywords)
+                                    .Field(p => p.FrameworkName)
+                                    .Field(p => p.PathwayName)
+                                    .Field(p => p.JobRoleItems.First().Title)
+                                    .Field(p => p.JobRoleItems.First().Description))
+                                .Query(formattedKeywords)))));
+
+            GetSortingOrder(searchDescriptor);
+
+            return searchDescriptor;
+        }
+
+        private SearchDescriptor<ApprenticeshipSearchResultsItem> GetKeywordSearchDescriptor(
+            int page, int take, string formattedKeywords)
+        {
+            var skip = (page - 1) * take;
+            var searchDescriptor = new SearchDescriptor<ApprenticeshipSearchResultsItem>()
+                    .Index(_applicationSettings.ApprenticeshipIndexAlias)
+                    .AllTypes()
+                    .Skip(skip)
+                    .Take(take)
+                    .Query(q => q
+                        .Bool(b => b
+                            .Filter(PublishedApprenticeship())
+                            .Must(
+                                MustBeStartedApprenticeship(),
+                                MustBeNonExpiredApprenticceship(),
+                                m => m
+                                    .Bool(mb => mb
+                                        .Should(
+                                            bs1 => bs1
+                                                .MultiMatch(mm => mm
+                                                    .Type(TextQueryType.BestFields)
+                                                    .Fields(fi => fi
+                                                        .Field(f => f.Title)
+                                                        .Field(f => f.Keywords)
+                                                        .Field(f => f.JobRoles)
+                                                        .Field(f => f.JobRoleItems.First().Title)
+                                                        .Field(f => f.JobRoleItems.First().Description))
+                                                    .Query(formattedKeywords)
+                                                    .MinimumShouldMatch("2<70%")
+                                                    .PrefixLength(3)),
+                                            bs2 => bs2
+                                                .MultiMatch(mm => mm
+                                                    .Type(TextQueryType.CrossFields)
+                                                    .Fields(fi => fi
+                                                        .Field(f => f.Title, 2)
+                                                        .Field(f => f.Keywords))
+                                                    .Query(formattedKeywords)
+                                                    .PrefixLength(3)))))));
+
+            GetSortingOrder(searchDescriptor);
+
+            return searchDescriptor;
+        }
+
+        private void GetSortingOrder(SearchDescriptor<ApprenticeshipSearchResultsItem> searchDescriptor)
+        {
+            if (Is<Elk5Feature>.Enabled)
+            {
+                searchDescriptor.Sort(s => s
+                    .Descending(SortSpecialField.Score)
+                    .Descending(f => f.TitleKeyword)
+                    .Descending(f => f.Level));
+            }
+            else
+            {
+                searchDescriptor.Sort(s => s
+                    .Descending(SortSpecialField.Score)
+                    .Descending(f => f.Title)
+                    .Descending(f => f.Level));
+            }
+        }
+
+        private static Func<QueryContainerDescriptor<ApprenticeshipSearchResultsItem>, QueryContainer> PublishedApprenticeship()
+        {
+            return f => f
+                .Term(t => t
+                    .Field(fi => fi.Published).Value(true));
+        }
+
+        private static Func<QueryContainerDescriptor<ApprenticeshipSearchResultsItem>, QueryContainer> MustBeNonExpiredApprenticceship()
+        {
+            return m1 => m1
+                .Bool(mb1 => mb1
+                    .Should(
+                        bs1 => bs1
+                                   .DateRange(r => r
+                                       .GreaterThanOrEquals(DateTime.Now)
+                                       .Field(f => f.EffectiveTo))
+                               || bs1
+                                   .Bool(bsb1 => bsb1
+                                       .MustNot(mn => mn
+                                           .Exists(e => e
+                                               .Field(f => f.EffectiveTo))))));
+        }
+
+        private static Func<QueryContainerDescriptor<ApprenticeshipSearchResultsItem>, QueryContainer> MustBeStartedApprenticeship()
+        {
+            return m0 => m0
+                .Bool(mb0 => mb0
+                    .Should(
+                        s0 => s0
+                                  .DateRange(r => r
+                                      .LessThanOrEquals(DateTime.Now)
+                                      .Field(f => f.EffectiveFrom))
+                              || s0
+                                  .Bool(b2 => b2
+                                      .MustNot(mn => mn
+                                          .Exists(e => e
+                                              .Field(f => f.EffectiveFrom))))));
+        }
+    }
+}
