a96574dcedb0eaf8c327640d3a8bbd2fb6c8285c
markcain <mark.cain@digital.education.gov.uk>
2018-03-23 15:48:56 +0000

    CI-535 minor layout change

--- src/SFA.DAS.Apprenticeships.Api.Types/Standard.cs
+++ src/SFA.DAS.Apprenticeships.Api.Types/Standard.cs
@@ -61,6 +61,7 @@ namespace SFA.DAS.Apprenticeships.Api.Types
         public DateTime? EffectiveTo { get; set; }
 
         public int StandardSectorCode { get; set; }
+
         public string StandardPageUri { get; set; }
     }
 }
