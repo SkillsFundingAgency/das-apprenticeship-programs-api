92240c66b22216da32061bcf0d61fa89a3675b68
froberg <christofer.froberg@gmail.com>
2018-04-24 15:52:03 +0100

    Added new acceptance tests project and some basic tests.

---
+++ src/Sfa.Das.ApprenticeshipInfoService.AcceptanceTests/Features/AssessmentOrganisations.feature
@@ -0,0 +1,17 @@
+﻿Feature: AssessmentOrganisations
+    Get ALL the AssessmentOrganisations
+
+
+Scenario Outline: Verify correct status code is returned
+   Given I send request to <uri>
+   Then I get response code <code> is returned
+
+Examples:
+| uri                                        | code     |                                
+| assessment-organisations                   | OK       |
+| assessment-test                            | NotFound |
+| assessment-organisations/EPA0001           | OK       |
+| assessment-organisations/standards/152     | OK       |
+| assessment-organisations/EPA0001/standards | OK       |
+| assessment-organisations/EPA1001           | NotFound |
+| assessmentorganisations/                   | NotFound |
---
+++ src/Sfa.Das.ApprenticeshipInfoService.AcceptanceTests/Helpers/HttpClientReqHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Configuration;
+using NUnit.Framework;
+
+namespace Sfa.Das.ApprenticeshipInfoService.AcceptanceTests.Helpers
+{
+    public class HttpClientReqHelper
+    {
+        HttpClient client;
+        private HttpResponseMessage response;
+        public HttpRequestMessage requestMessage;
+
+        public HttpClientReqHelper(string uri)
+        {
+            RunAsync(uri);
+        }
+
+        private void RunAsync(string uri)
+        {
+            var url = ConfigurationManager.AppSettings.Get("ApprenticeshipInfoService");
+            client = new HttpClient { BaseAddress = new Uri(url) };
+            requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            requestMessage.Headers.Add("Accept", "application/json");
+        }
+
+        public async Task<HttpResponseMessage> ExecuteHttpGetRequest(HttpRequestMessage request)
+        {
+            response = await client.SendAsync(request);
+            return response;
+        }
+
+        public void EnsureAppropriateResponseCode(string code)
+        {
+            String StatusCode = response.StatusCode.ToString();
+            Assert.AreEqual(code, StatusCode);
+        }
+
+        public async Task<T> GetBody<T>()
+        {
+            var s = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+    }
+}
