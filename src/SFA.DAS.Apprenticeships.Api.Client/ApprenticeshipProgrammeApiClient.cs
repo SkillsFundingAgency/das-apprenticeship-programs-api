55db8dec3727db43d8d4a77d9ea60cacc1ddcd95
josevi.agullo <josevi.agullo@gmail.com>
2018-07-24 13:45:35 +0100

    CI-678 Adding client method for apprenticeship search

--- src/SFA.DAS.Apprenticeships.Api.Client/ApprenticeshipProgrammeApiClient.cs
+++ src/SFA.DAS.Apprenticeships.Api.Client/ApprenticeshipProgrammeApiClient.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Sfa.Das.ApprenticeshipInfoService.Core.Models;
 using SFA.DAS.Apprenticeships.Api.Types;
 
 namespace SFA.DAS.Apprenticeships.Api.Client
@@ -19,12 +20,28 @@ namespace SFA.DAS.Apprenticeships.Api.Client
             }
         }
 
-        public async Task<IEnumerable<ApprenticeshipSummary>> GetAsync()
-        {
-            using (var request = new HttpRequestMessage(HttpMethod.Get, $"/apprenticeship-programmes"))
-            {
-                return await RequestAndDeserialiseAsync<IEnumerable<ApprenticeshipSummary>>(request);
-            }
-        }
-    }
+		public async Task<IEnumerable<ApprenticeshipSummary>> GetAsync()
+		{
+			using (var request = new HttpRequestMessage(HttpMethod.Get, $"/apprenticeship-programmes"))
+			{
+				return await RequestAndDeserialiseAsync<IEnumerable<ApprenticeshipSummary>>(request);
+			}
+		}
+
+		public IEnumerable<ApprenticeshipSearchResultsItem> Search(string keywords, int page)
+		{
+			using (var request = new HttpRequestMessage(HttpMethod.Get, $"/apprenticeship-programmes/search?keywords={keywords}&page={page}"))
+			{
+				return RequestAndDeserialise<IEnumerable<ApprenticeshipSearchResultsItem>>(request);
+			}
+		}
+
+		public async Task<IEnumerable<ApprenticeshipSearchResultsItem>> SearchAsync(string keywords, int page)
+		{
+			using (var request = new HttpRequestMessage(HttpMethod.Get, $"/apprenticeship-programmes/search?keywords={keywords}&page={page}"))
+			{
+				return await RequestAndDeserialiseAsync<IEnumerable<ApprenticeshipSearchResultsItem>>(request);
+			}
+		}
+	}
 }
