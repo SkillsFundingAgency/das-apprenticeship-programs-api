ef4337672139d92e8cf69d24f5890d187e5e86da
Corey <corey.faulconbridge@digital.education.gov.uk>
2018-09-18 15:49:48 +0100

    PF-25 Add Feedback to Provider

---
+++ src/SFA.DAS.Apprenticeships.Api.Types/Providers/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Apprenticeships.Api.Types.Providers
+{
+    public class Feedback
+    {
+        public ICollection<ProviderAttribute> Strengths { get; set; }
+        public ICollection<ProviderAttribute> Weaknesses { get; set; }
+        public int ExcellentFeedbackCount { get; set; }
+        public int GoodFeedbackCount { get; set; }
+        public int PoorFeedbackCount { get; set; }
+        public int VeryPoorFeedbackCount { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Apprenticeships.Api.Types/Providers/ProviderAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Apprenticeships.Api.Types.Providers
+{
+    public class ProviderAttribute
+    {
+        public string Name { get; set; }
+
+        public int Count { get; set; }
+    }
+}
