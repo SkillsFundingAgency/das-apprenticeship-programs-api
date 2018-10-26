a1a6fd5be75e2dc31bc082d852e4f824ec760c96
Corey <corey.faulconbridge@digital.education.gov.uk>
2018-10-26 21:03:18 +0100

    [PF-179] Change how provider attributes are stored within feedback

--- src/SFA.DAS.Apprenticeships.Api.Types/Providers/Feedback.cs
+++ src/SFA.DAS.Apprenticeships.Api.Types/Providers/Feedback.cs
@@ -4,8 +4,7 @@ namespace SFA.DAS.Apprenticeships.Api.Types.Providers
 {
     public class Feedback
     {
-        public ICollection<ProviderAttribute> Strengths { get; set; }
-        public ICollection<ProviderAttribute> Weaknesses { get; set; }
+        public ICollection<ProviderAttribute> ProviderAttributes { get; set; }
         public int ExcellentFeedbackCount { get; set; }
         public int GoodFeedbackCount { get; set; }
         public int PoorFeedbackCount { get; set; }
