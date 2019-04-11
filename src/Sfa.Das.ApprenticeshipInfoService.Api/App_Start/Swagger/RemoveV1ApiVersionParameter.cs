b5ab41234834397adf3da5b7b24d721ff5bbf650
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-04-11 15:23:55 +0100

    Update Swashbuckle

--- src/Sfa.Das.ApprenticeshipInfoService.Api/App_Start/Swagger/RemoveV1ApiVersionParameter.cs
+++ src/Sfa.Das.ApprenticeshipInfoService.Api/App_Start/Swagger/RemoveV1ApiVersionParameter.cs
@@ -1,23 +1,23 @@
-﻿//using System.Linq;
-//using System.Web.Http.Description;
-//using Swashbuckle.Swagger;
+﻿using System.Linq;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
 
-//namespace Sfa.Das.ApprenticeshipInfoService.Api.Swagger
-//{
+namespace Sfa.Das.ApprenticeshipInfoService.Api.Swagger
+{
 
-//    internal class RemoveV1ApiVersionParameter : IOperationFilter
-//    {
-//        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
-//        {
-//            if (apiDescription.GetGroupName() == "v1" && operation.parameters != null)
-//            {
-//                var versionParameter = operation.parameters.SingleOrDefault(x => x.name == "api-version");
+    internal class RemoveV1ApiVersionParameter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (apiDescription.GetGroupName() == "v1" && operation.parameters != null)
+            {
+                var versionParameter = operation.parameters.SingleOrDefault(x => x.name == "api-version");
 
-//                if (versionParameter != null)
-//                {
-//                    operation.parameters.Remove(versionParameter);
-//                }
-//            }
-//        }
-//    }
-//}
+                if (versionParameter != null)
+                {
+                    operation.parameters.Remove(versionParameter);
+                }
+            }
+        }
+    }
+}
