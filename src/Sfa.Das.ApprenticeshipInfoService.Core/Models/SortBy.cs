7e75985b28f2ea3133554562d1fc7c5f6e0a07e2
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-09-05 16:05:59 +0100

    Remove unused class

--- src/Sfa.Das.ApprenticeshipInfoService.Core/Models/SortBy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sfa.Das.ApprenticeshipInfoService.Core.Models
-{
-    public sealed class SortBy
-    {
-        public int Order { get; set; }
-
-        public Coordinate Coordinate { get; set; }
-    }
-}
